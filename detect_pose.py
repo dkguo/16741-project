27fbf5cae778469211d494ec5c23dad6f0f7e225
Dingkun Guo <guodk@umich.edu>
2022-12-03 16:16:56 -0500

    add files

---
+++ detect_pose.py
@@ -0,0 +1,32 @@
+import glob
+import json
+
+import cv2
+from tqdm import tqdm
+
+from apriltag_detection import detect_april_tag, apriltag_detect_error_thres
+
+
+if __name__ == '__main__':
+
+    image_paths = sorted(glob.glob(f'/Users/gdk/Downloads/pushing/scene_2212031401/camera_03_827112072509/rgb/*.png'))
+    camera_params = (615.587890625, 615.9234619140625, 326.09588623046875, 242.5946044921875)
+    tag_size = 0.08
+
+    poses = []
+    for image_path in tqdm(image_paths):
+        image = cv2.imread(image_path)
+        poses_errs, overlay = detect_april_tag(image, camera_params, tag_size)
+
+        found_pose = False
+        for tag_id, pose, error in poses_errs:
+            if tag_id == 0:
+                poses.append(pose.tolist())
+                found_pose = True
+                break
+        if not found_pose:
+            poses.append(None)
+
+    print(poses)
+    with open('/Users/gdk/Downloads/pushing/scene_2212031401/camera_03_827112072509/april_tag_poses.json', 'w') as f:
+        json.dump(poses, f, indent=4)
--- kalman_filter.py
+++ kalman_filter.py
@@ -2,7 +2,7 @@ import json
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.spatial.transform import Rotation
 
 def load_time(time_file_path):
     with open(time_file_path) as f:
@@ -44,40 +44,115 @@ def load_object_poses(file_path):
     return object_poses
 
 
-def kalman_filter(times, poses):
+def kalman_filter(poses, dt=1/30):
     """
-    :param times: list of time from frame 0 to end
-    :param poses: list of pose from frame 0 to end
+    Apply linear kalman filter on poses.
+    Poses are converted to position and euler angles: [x y z ex ey ez]
+    :param poses: list of pose in homogenous matrix from frame 0 to end
+    :param dt: delta time between two frames
 
     :return zs: estimated poses
     """
-    pass
+
+    positions = []
+    angles = []
+    for i, pose in enumerate(poses):
+        if pose is not None:
+            positions.append(pose[:3, -1])
+            print(Rotation.from_matrix(pose[:3, :3]).as_euler('XYZ', degrees=True))
+            angles.append(Rotation.from_matrix(pose[:3, :3]).as_euler('XYZ'))
+        else:
+            positions.append(np.array([np.nan, np.nan, np.nan]))
+            angles.append(np.array([np.nan, np.nan, np.nan]))
+
+    # angles = np.array(angles)
+    # angles[angles < 0] += 2 * np.pi
+    # plt.plot(angles[:, 0])
+    # plt.plot(angles[:, 1])
+    # plt.plot(angles[:, 2])
+    # plt.show()
+
+
+    # exit()
+    x0 = np.r_[xyzs[0][0], 0, 0, xyzs[0][1], 0, 0, xyzs[0][2], 0, 0]
+    P0 = np.diag([500] * 9)
+
+    dt = 1 / 15
+
+    F1 = np.array([[1, dt, 0.5 * dt ** 2],
+                   [0, 1, dt],
+                   [0, 0, 1]])
+    F = np.zeros([9, 9])
+    F[:3, :3] = F1
+    F[3:6, 3:6] = F1
+    F[-3:, -3:] = F1
+
+    H = np.zeros([3, 9])
+    H[0, 0] = 1
+    H[1, 3] = 1
+    H[2, 6] = 1
+
+    # measurement uncertainty
+    ra = 1.0
+    R = np.diag([ra] * 3)
+
+    # process noise
+    Q1 = np.array([[dt ** 4 / 4, dt ** 3 / 2, dt ** 2 / 2],
+                   [dt ** 3 / 2, dt ** 2, dt],
+                   [dt ** 2 / 2, dt, 1]])
+    Q = np.zeros([9, 9])
+    Q[:3, :3] = Q1
+    Q[3:6, 3:6] = Q1
+    Q[-3:, -3:] = Q1
+    sq_sigma_a = 100
+    Q = sq_sigma_a * Q
+
+    n = len(xyzs)
+    x = [[None] * n] * (n + 1)
+    P = [[None] * n] * (n + 1)
+    K = [None] * n
+    z = [None] * n
+    x[0][0] = x0
+    P[0][0] = P0
+    x[1][0] = F @ x[0][0]
+    P[1][0] = F @ P[0][0] @ F.T + Q
+    for i in range(1, len(xyzs)):
+        z[i] = xyzs[i]
+
+        K[i] = (P[i][i - 1] @ H.T) @ np.linalg.pinv(H @ P[i][i - 1] @ H.T + R)
+        x[i][i] = x[i][i - 1] + K[i] @ (z[i] - H @ x[i][i - 1])
+        P[i][i] = (np.eye(9) - K[i] @ H) @ P[i][i - 1] @ (np.eye(9) - K[i] @ H).T + K[i] @ R @ K[i].T
+
+        x[i + 1][i] = F @ x[i][i]
+        P[i + 1][i] = F @ P[i][i] @ F.T + Q
+
+
+
 
 
 def plot_poses(poses):
     coods = []
-    for pose in poses:
+    for pose in poses[:100]:
         if pose is not None:
             p_object = pose[:3, 3]
             R = pose[:3, :3]
-            p_x = R @ [1, 0, 0]
-            p_y = R @ [0, 1, 0]
-            p_z = R @ [0, 0, 1]
+            p_x = R @ [0.01, 0, 0]
+            p_y = R @ [0, 0.01, 0]
+            p_z = R @ [0, 0, 0.01]
             cood = np.r_[[np.r_[p_object, p_x],
                           np.r_[p_object, p_y],
                           np.r_[p_object, p_z]]]
             coods.append(cood)
 
-    coods = np.array(coods).reshape((-1, 6))
-    X, Y, Z, U, V, W = zip(*coods)
+    coods = np.array(coods)
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.quiver(X, Y, Z, U, V, W)
-    ax.set_xlim([-2, 2])
-    ax.set_ylim([-2, 2])
-    ax.set_zlim([-2, 2])
+    for i, c in zip(range(3), ['b', 'r', 'g']):
+        s = coods[:, i, :].reshape((-1, 6))
+        X, Y, Z, U, V, W = zip(*s)
+        ax.quiver(X, Y, Z, U, V, W, color=c)
     plt.show()
-
 
 
 if __name__ == '__main__':
@@ -85,6 +160,15 @@ if __name__ == '__main__':
     object_poses = load_object_poses('./object_poses.json')
 
     object_est_poses = {}
-    for object_id, poses in object_poses.items():
-        plot_poses(poses)
+    poses = object_poses['13']
+    for i in range(len(poses)):
+        if poses[i] is not None:
+            first_none = i
+    for i in range(len(poses)):
+        if poses[i] is not None:
+            print(poses[i])
+            poses[i] = np.linalg.inv(poses[first_none]) @ poses[i]
+    # for object_id, poses in object_poses.items():
+    # plot_poses(poses)
+    filtered_poses = kalman_filter(poses)
         # object_est_poses[object_id] = kalman_filter(times, poses)
